9fdaf2140d0ee2f402263ba84e1a7df5240c1433
Sreenivasulugoddela <vsree9227@gmail.com>
2022-12-08 16:30:33 +0530

    added responseive to nav bar

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
-import {IconButton} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
+import {Drawer, IconButton, ListItemButton, ListItemIcon, ListItemText,List} from '@mui/material'
+const PAGES=["Products","Services","About us","Contacts us","Login","Logout"]
+import MenuIcon from '@mui/icons-material/Menu';
 export const DrawerComp = () => {
+    const [drawer,setDrawer]=useState(false)
+
   return (
     <>
-    <IconButton sx={{color:"black",marginLeft:"auto"}} >
+    <Drawer open={drawer} onClose={()=>setDrawer(false)}>
+      <List>
+        
+          {PAGES.map((page,index)=>(
+            <ListItemButton>
+            <ListItemIcon>
+            
+              <ListItemText  key={index}>{page}</ListItemText>
+              </ListItemIcon>
+        </ListItemButton>
+           ))}
+        
+      </List>
+    </Drawer>
+    
+    <IconButton sx={{color:"white",marginLeft:"auto"}} onClick={()=>setDrawer(!drawer)}>
         <MenuIcon/>
     </IconButton>
     </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,8 @@
 import { AppBar, Button, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useState } from "react";
-import Album from "./HomePage";
 import { DrawerComp } from "./Drawer";
-
+const PAGES=["Products","Services","About us","Contacts us"]
 const Navbar = () => {
   const [value,setValue]=useState(0)
   const theme=useTheme()
@@ -11,24 +10,22 @@ const Navbar = () => {
   
   return (
     <div>
-      <AppBar sx={{background:"#1976d2"}}>
+      <AppBar sx={{background:"#6d7bab"}}>
         <Toolbar>
          <ShoppingCartIcon/>
          {
           isMatch?(
-            <>
             <DrawerComp/>
-            </>
-          ):<>
-          <Tabs textColor="inherit" value={value} onChange={(e,value)=>setValue(value)} indicatorColor="secondary">
-          <Tab label="Home"/>
-          <Tab label="Products"/>
-          <Tab label="Contact us"/>
-          <Tab label="About us"/>
+          ):
+          (<>
+          <Tabs textColor="inherit" sx={{marginLeft:"auto"}} value={value} onChange={(e,value)=>setValue(value)} indicatorColor="primary">
+           {PAGES.map((page,index)=>(
+              <Tab label={page} key={index}/>
+           ))}
          </Tabs>
-         <Button sx={{marginLeft:"auto",color:"white" ,backgroundColor:"#1976d20a"}} variant="contained">Login</Button>
+         <Button sx={{marginLeft:"auto"}} variant="contained">Login</Button>
          <Button sx={{marginLeft:"10px"}} variant="contained">Signup</Button>
-          </>
+          </>)
          }
          
          
